2468044785e662c3d98618072b87fb80e2befb3c
briancanspit@gmail.com <briancanspit@gmail.com>
2019-11-01 20:16:42 +0300

    Improved index page bootstrap layout

--- src/components/content/indexPage.js
+++ src/components/content/indexPage.js
@@ -3,13 +3,17 @@ import Style from "./styles/index.module.css"
 import CoffeeImg from "../../images/coffee.svg"
 
 const MainContent = () => {
+  let leftStyles = `col-md-6 ${Style.left}`
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-md-6">
-          <h2 className={Style.headerText}>
+        <div className={leftStyles}>
+          <h4>Stir. Serve. Taste.</h4>
+          <h2 className={Style.header}>
             Welcome to London's finest brewer of coffee
           </h2>
+          <button className="btn btn-warning btn-lg mr-2">Get Started</button>
+          <button className="btn btn-warning btn-lg">Learn More</button>
         </div>
         <div className="col-md-6">
           <img className={Style.coffeeImg} src={CoffeeImg} alt="Coffee Cup" />
