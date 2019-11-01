416b93c709385c0c84e3c025f4f2e0e607c2e337
briancanspit@gmail.com <briancanspit@gmail.com>
2019-11-01 18:54:27 +0300

    Revamped interface - changed paths and nav names

---
+++ src/components/content/servicesPage.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const MainContent = () => {
+  return (
+    <div>
+      <h1>Services Page</h1>
+      <p>Everything goes here</p>
+    </div>
+  )
+}
+
+export default MainContent
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import MainContent from "../components/content/aboutPage"
+
+const RegisterPage = () => {
+  return (
+    <Layout>
+      <MainContent />
+    </Layout>
+  )
+}
+
+export default RegisterPage
