3efadcbea04e8b34d95ded5cae494f6ab57d966e
briancanspit@gmail.com <briancanspit@gmail.com>
2019-11-01 23:33:24 +0300

    Added preloader and fade in effect on coffee img

--- src/components/content/indexPage.js
+++ src/components/content/indexPage.js
@@ -1,26 +1,70 @@
-import React from "react"
+import React, { Component } from "react"
+import Typist from "react-typist"
 import Style from "./styles/index.module.css"
 import CoffeeImg from "../../images/coffee.svg"
+import "../../../node_modules/react-typist/dist/Typist.css"
 
-const MainContent = () => {
+const Content = () => {
   let leftStyles = `col-md-6 ${Style.left}`
   return (
-    <div className="container-fluid">
+    <div className="container-fluid fade-in-right">
       <div className="row">
-        <div className={leftStyles}>
-          <h4>Stir. Serve. Taste.</h4>
-          <h2 className={Style.header}>
-            Welcome to London's finest brewer of coffee
-          </h2>
-          <button className="btn btn-warning btn-lg mr-2">Get Started</button>
-          <button className="btn btn-warning btn-lg">Learn More</button>
-        </div>
+        <div className={leftStyles}></div>
         <div className="col-md-6">
           <img className={Style.coffeeImg} src={CoffeeImg} alt="Coffee Cup" />
         </div>
       </div>
     </div>
   )
+}
+
+const Loader = () => {
+  return (
+    <React.Fragment>
+      <div className={Style.typer}>
+        <Typist avgTypingDelay={40} startDelay={500}>
+          <h1 className={Style.mainHeader}>Here's the thing.</h1>
+          <Typist.Backspace count={40} delay={250} />
+          <h1 className={Style.mainHeader}>Everybody loves coffee.</h1>
+          <Typist.Backspace count={40} delay={250} />
+          <h1 className={Style.mainHeader}>We make it.</h1>
+          <Typist.Backspace count={40} delay={250} />
+          <h1 className={Style.mainHeader}>
+            Welcome to <span style={{ fontWeight: "bold" }}>Brew</span>.
+          </h1>
+        </Typist>
+      </div>
+      <div className={Style.preloader}>
+        <img src={require("../../images/preloader.svg")} alt="Preloader" />
+      </div>
+    </React.Fragment>
+  )
+}
+
+class MainContent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  handleContent = () => {
+    this.setState({
+      loaded: true,
+    })
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      setTimeout(() => {
+        this.handleContent()
+      }, 11000)
+      return <div>{Loader()}</div>
+    } else {
+      return <div>{Content()}</div>
+    }
+  }
 }
 
 export default MainContent
