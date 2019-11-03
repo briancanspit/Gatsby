bf127edec7c80bb8197944b915100dc605399e67
briancanspit@gmail.com <briancanspit@gmail.com>
2019-11-03 22:12:48 +0300

    Removed preloader, modified typer on index page

--- src/components/content/indexPage.js
+++ src/components/content/indexPage.js
@@ -9,68 +9,64 @@ import { faMugHot } from "@fortawesome/free-solid-svg-icons"
 import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons"
 
 const Content = () => {
+  let typeText = `badge badge-warning ${Style.mainHeader} ${Style.typerStyle}`
   let leftStyles = `col-md-6 fade-in-left ${Style.left}`
   let btnStyle1 = `btn btn-lg ${Style.button} mr-2`
   let btnStyle2 = `btn btn-lg ${Style.button}`
   let btnText = `mr-2 ${Style.buttonText}`
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className={leftStyles}>
-          <h1 className={Style.tagline}>Espresso Heaven</h1>
-          <h4 className={Style.contentText}>
-            We are the leading provider of packaged coffee in Kenya, and operate
-            in over 150 locations. Get your coffee delivered to you for{" "}
-            <em>free</em>! Make an order today or explore our site to see what
-            we do.
-          </h4>
-          <br />
-          <br />
-          <Link to="/login">
-            <button className={btnStyle1}>
-              <span className={btnText}>Make An Order</span>
-              <FontAwesomeIcon
-                className={Style.fontAwesome}
-                icon={faMugHot}
-                color="#dfddc7"
-              />
-            </button>
-          </Link>
-          <Link to="/services">
-            <button className={btnStyle2}>
-              <span className={btnText}>See Our Works</span>
-              <FontAwesomeIcon
-                className={Style.fontAwesome}
-                icon={faArrowAltCircleRight}
-                color="#dfddc7"
-              />
-            </button>
-          </Link>
-        </div>
-        <div className="col-md-6 fade-in-right">
-          <img className={Style.coffeeImg} src={CoffeeImg} alt="Coffee Cup" />
-        </div>
-      </div>
-    </div>
-  )
-}
-
-const Loader = () => {
+  let container = `container-fluid ${Style.container}`
   return (
     <React.Fragment>
-      <div className={Style.typer}>
-        <Typist avgTypingDelay={65} startDelay={500}>
-          <h1 className={Style.mainHeader}>Running late?</h1>
-          <Typist.Backspace count={25} delay={255} />
-          <h1 className={Style.mainHeader}>Need some coffee?</h1>
-          <Typist.Backspace count={30} delay={260} />
-          <h1 className={Style.mainHeader}>
-            Introducing <span style={{ fontWeight: "bold" }}>Brew</span>.
-          </h1>
-        </Typist>
-      </div>
-      <div className={Style.preloader}>
-        <img src={require("../../images/preloader.svg")} alt="Preloader" />
+      <Typist avgTypingDelay={65} startDelay={1600} className={Style.typer}>
+        <h1 className={typeText}>Running late for work?</h1>
+        <Typist.Backspace count={22} delay={255} />
+        <Typist.Delay ms={800} />
+        <h1 className={typeText}>Forgot your morning coffee?</h1>
+        <Typist.Backspace count={27} delay={260} />
+        <Typist.Delay ms={800} />
+        <h1 className={typeText}>
+          Introducing <span style={{ fontWeight: "bold" }}>Brew</span> -{" "}
+          <Typist.Delay ms={300} /> coffee delivery made easy! 🥤
+        </h1>
+      </Typist>
+      <div className={container}>
+        <div className="row">
+          <div className={leftStyles}>
+            <h3 className={Style.tagline}>Stir. Pack. Serve.</h3>
+            <h2 className={Style.tagTwo}>Experience ultimate fudginess</h2>
+            <h4 className={Style.contentText}>
+              Let's face it - we all run late once in a while, making us not
+              have that warm cup of morning coffee that we so desire. Why let
+              that get in your way? We prepare and package the best coffee in
+              Kenya and deliver it for <strong>free</strong>!
+            </h4>
+            <br />
+            <br />
+            <Link to="/login">
+              <button className={btnStyle1}>
+                <span className={btnText}>Make An Order</span>
+                <FontAwesomeIcon
+                  className={Style.fontAwesome}
+                  icon={faMugHot}
+                  color="#dfddc7"
+                />
+              </button>
+            </Link>
+            <Link to="/services">
+              <button className={btnStyle2}>
+                <span className={btnText}>See Our Works</span>
+                <FontAwesomeIcon
+                  className={Style.fontAwesome}
+                  icon={faArrowAltCircleRight}
+                  color="#dfddc7"
+                />
+              </button>
+            </Link>
+          </div>
+          <div className="col-md-6 fade-in-right">
+            <img className={Style.coffeeImg} src={CoffeeImg} alt="Coffee Cup" />
+          </div>
+        </div>
       </div>
     </React.Fragment>
   )
@@ -79,26 +75,11 @@ const Loader = () => {
 class MainContent extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      loaded: false,
-    }
-  }
-
-  handleContent = () => {
-    this.setState({
-      loaded: true,
-    })
+    this.state = {}
   }
 
   render() {
-    if (!this.state.loaded) {
-      setTimeout(() => {
-        this.handleContent()
-      }, 9300)
-      return <div>{Loader()}</div>
-    } else {
-      return <div>{Content()}</div>
-    }
+    return <div>{Content()}</div>
   }
 }
 
