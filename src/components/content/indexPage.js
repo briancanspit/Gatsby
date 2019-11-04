28da3bb33cc86bafabcaa5ddf832c9d564419533
briancanspit@gmail.com <briancanspit@gmail.com>
2019-11-04 17:11:11 +0300

    Fixed glitches cause of coffeeImg padding; added mobile margin between buttons

--- src/components/content/indexPage.js
+++ src/components/content/indexPage.js
@@ -9,32 +9,33 @@ import { faMugHot } from "@fortawesome/free-solid-svg-icons"
 import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons"
 
 const Content = () => {
-  let typeText = `badge badge-warning ${Style.mainHeader} ${Style.typerStyle}`
-  let leftStyles = `col-md-6 col-sm-12 fade-in-left ${Style.left}`
-  let btnStyle1 = `btn btn-lg ${Style.button} mr-2 mb-2`
-  let btnStyle2 = `btn btn-lg ${Style.button}`
-  let btnText = `mr-2 ${Style.buttonText}`
-  let container = `container-fluid ${Style.container}`
+  let typeText = `badge badge-warning ${Style.mainHeader} ${Style.typerStyle}`,
+    leftStyles = `col-md-6 fade-in-left ${Style.left}`,
+    btnStyle1 = `btn btn-lg ${Style.button} mr-2`,
+    btnStyle2 = `btn btn-lg ${Style.button}`,
+    btnText = `mr-2 ${Style.buttonText}`,
+    visibleOnMobile = `${Style.mobileMargin} visible-xs`,
+    container = `container-fluid ${Style.container}`
   return (
     <React.Fragment>
+      <Typist
+        avgTypingDelay={65}
+        startDelay={1200}
+        cursor={{ show: false, blink: false, hideWhenDone: true }}
+        className={Style.typer}
+      >
+        <p className={typeText}>Running late for work?</p>
+        <Typist.Backspace count={22} delay={255} />
+        <Typist.Delay ms={800} />
+        <p className={typeText}>Forgot your morning coffee?</p>
+        <Typist.Backspace count={27} delay={260} />
+        <Typist.Delay ms={800} />
+        <p className={typeText}>
+          Welcome to <span style={{ fontWeight: "bold" }}>Brew</span>{" "}
+          <Typist.Delay ms={50} /> 🥤
+        </p>
+      </Typist>
       <div className={container}>
-        <Typist
-          avgTypingDelay={65}
-          startDelay={1200}
-          cursor={{ show: false, blink: false, hideWhenDone: true }}
-          className={Style.typer}
-        >
-          <p className={typeText}>Running late for work?</p>
-          <Typist.Backspace count={22} delay={255} />
-          <Typist.Delay ms={800} />
-          <p className={typeText}>Forgot your morning coffee?</p>
-          <Typist.Backspace count={27} delay={260} />
-          <Typist.Delay ms={800} />
-          <p className={typeText}>
-            Welcome to <span style={{ fontWeight: "bold" }}>Brew</span>{" "}
-            <Typist.Delay ms={50} /> 🥤
-          </p>
-        </Typist>
         <div className="row">
           <div className={leftStyles}>
             <h3 className={Style.tagline}>Stir. Pack. Serve.</h3>
@@ -58,6 +59,7 @@ const Content = () => {
                 />
               </button>
             </Link>
+            <div className={visibleOnMobile}></div>
             <Link to="/services">
               <button className={btnStyle2}>
                 <span className={btnText}>See Our Works</span>
@@ -70,7 +72,7 @@ const Content = () => {
               </button>
             </Link>
           </div>
-          <div className="col-md-6 col-sm-12 fade-in-right">
+          <div className="col-md-6 fade-in-right">
             <img className={Style.coffeeImg} src={CoffeeImg} alt="Coffee Cup" />
           </div>
         </div>
