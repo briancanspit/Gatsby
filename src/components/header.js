5cb63103b3abc5595bd41e4b51ae9a5ea4bf5e14
briancanspit@gmail.com <briancanspit@gmail.com>
2019-11-04 18:37:01 +0300

    Got navbar toggle button to work

--- src/components/header.js
+++ src/components/header.js
@@ -7,10 +7,25 @@ class Header extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      active: false,
+      collapsed: true,
     }
   }
+
+  handleToggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    })
+  }
+
   render() {
+    const collapsed = this.state.collapsed
+    const classOne = collapsed
+      ? "collapse navbar-collapse"
+      : "collapse navbar-collapse show"
+    const classTwo = collapsed
+      ? "navbar-toggler navbar-toggler-right collapsed"
+      : "navbar-toggler navbar-toggler-right"
+
     const linkClass = `nav-link ${HeaderStyle.link}`
     const navClass = `navbar navbar-expand-lg fixed-top navbar-dark bg-dark`
     const loginStyle = `nav-item ${HeaderStyle.loginButton}`
@@ -24,17 +39,21 @@ class Header extends Component {
               <span className={HeaderStyle.logoName}>BREW</span>
             </Link>
             <button
-              className="navbar-toggler"
+              className={`navbar-toggler ${classTwo}`}
               type="button"
               data-toggle="collapse"
               data-target="#navbarResponsive"
               aria-controls="navbarResponsive"
               aria-expanded="false"
               aria-label="Toggle navigation"
+              onClick={this.handleToggle}
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
+            <div
+              className={`collapse navbar-collapse ${classOne}`}
+              id="navbarResponsive"
+            >
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item" style={{ paddingRight: 25 }}>
                   <Link className={linkClass} to="/about">
