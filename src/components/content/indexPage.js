715580cd4ba53501a64aa1de64adc39038be5886
briancanspit@gmail.com <briancanspit@gmail.com>
2019-11-04 05:11:17 +0300

    Returned badge classnames to typist component

--- src/components/content/indexPage.js
+++ src/components/content/indexPage.js
@@ -9,7 +9,7 @@ import { faMugHot } from "@fortawesome/free-solid-svg-icons"
 import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons"
 
 const Content = () => {
-  let typeText = ` ${Style.mainHeader} ${Style.typerStyle}`
+  let typeText = `badge badge-warning ${Style.mainHeader} ${Style.typerStyle}`
   let leftStyles = `col-md-6 fade-in-left ${Style.left}`
   let btnStyle1 = `btn btn-lg ${Style.button} mr-2`
   let btnStyle2 = `btn btn-lg ${Style.button}`
