e97434f9612eaef4c1356b4eb7a00378d7fa01de
briancanspit@gmail.com <briancanspit@gmail.com>
2019-11-04 21:01:58 +0300

    Increased height of div containing map

--- src/components/content/contactPage.js
+++ src/components/content/contactPage.js
@@ -68,9 +68,10 @@ class MainContent extends Component {
       return null
     }
   }
+
   render() {
-    
-    const mapStyles = { width: "650px", height: "450px" }
+    const mapStyles = { width: "100%", height: "90%" }
+    let mapDiv = `col-md-7 col-sm-12 ${Styles.mapDiv}`
     let card = `card mb-3 ${Styles.card}`
     let cardTitle = `card-title text-center ${Styles.cardTitle}`
     let submitBtn = ` btn btn-block ${Styles.submitButton} mb-2`
@@ -165,7 +166,7 @@ class MainContent extends Component {
                 </p>
               </div>
             </div>
-            <div className="col-md-7 col-sm-12">
+            <div className={mapDiv}>
               {this.state.isPopoverOpen ? (
                 <span className="alert alert-success mb-4">
                   Haile Selassie Ave, P.O. BOX 1234-30200, Nairobi KE
