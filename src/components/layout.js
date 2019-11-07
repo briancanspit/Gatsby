92c4534f099bbff7b2eb4d6b395aa10b53877dc3
briancanspit@gmail.com <briancanspit@gmail.com>
2019-11-07 17:34:30 +0300

    Removed react-helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Helmet } from "react-helmet"
 import Header from "./header"
 import LayoutStyle from "../styles/layout.module.css"
 
@@ -8,15 +7,6 @@ const Layout = props => {
     <React.Fragment>
       <Header />
       <div className={LayoutStyle.content}>{props.children}</div>
-      <Helmet>
-        <script
-          type="text/javascript"
-          id="hs-script-loader"
-          async
-          defer
-          src="https://js.hs-scripts.com/6685693.js"
-        ></script>
-      </Helmet>
     </React.Fragment>
   )
 }
