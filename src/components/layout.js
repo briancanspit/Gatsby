acfadc620fd4c6d6807f4aa1cb7613991b7d7fc7
briancanspit@gmail.com <briancanspit@gmail.com>
2019-11-07 17:07:37 +0300

    Removed commented out code from index page; experimenting with react-helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Header from "./header"
 import LayoutStyle from "../styles/layout.module.css"
 
@@ -7,6 +8,15 @@ const Layout = props => {
     <React.Fragment>
       <Header />
       <div className={LayoutStyle.content}>{props.children}</div>
+      <Helmet>
+        <script
+          type="text/javascript"
+          id="hs-script-loader"
+          async
+          defer
+          src="https://js.hs-scripts.com/6685693.js"
+        ></script>
+      </Helmet>
     </React.Fragment>
   )
 }
